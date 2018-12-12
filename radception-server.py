1c94993da52968da1467c946928846c05ec316af
Kevin Tang <kevtang@mit.edu>
2018-12-11 23:50:54 -0500

    Changed server.

--- radception-server.py
+++ radception-server.py
@@ -3,10 +3,11 @@ from firebase_admin import db
 from firebase_admin import credentials
 import json
 import flask
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 app = flask.Flask(__name__)
 CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 cred = credentials.Certificate('./radception-database-credentials.json')
 firebase_admin.initialize_app(cred, options={
@@ -16,20 +17,24 @@ USERS = db.reference('users')
 DEVICES = db.reference('devices')
 
 @app.route("/")
+@cross_origin()
 def hello():
     return "Hello World!"
 
 @app.route('/users', methods=['POST'])
+@cross_origin()
 def create_user():
     req = flask.request.json
     user = USERS.child(req["username"]).set(req)
     return flask.jsonify({'id': user.key}), 201
 
 @app.route('/users/<id>')
+@cross_origin()
 def read_user(id):
     return flask.jsonify(_ensure_user(id))
 
 @app.route('/users/<id>', methods=['PUT'])
+@cross_origin()
 def update_user(id):
     _ensure_user(id)
     req = flask.request.json
@@ -37,6 +42,7 @@ def update_user(id):
     return flask.jsonify({'success': True})
 
 @app.route('/users/<id>', methods=['DELETE'])
+@cross_origin()
 def delete_user(id):
     _ensure_user(id)
     USERS.child(id).delete()
@@ -51,16 +57,19 @@ def _ensure_user(id):
 current_readings = [[], 0, 0]
 
 @app.route('/devices', methods=['POST'])
+@cross_origin()
 def create_device():
     req = flask.request.json
     device = DEVICES.child(req["device_id"]).set(req)
     return flask.jsonify({'id': device.key}), 201
 
 @app.route('/devices/<id>')
+@cross_origin()
 def read_device(id):
     return flask.jsonify(_ensure_device(id))
 
 @app.route('/devices/<id>', methods=['PUT'])
+@cross_origin()
 def update_device(id):
     _ensure_device(id)
     req = flask.request.json
@@ -79,6 +88,7 @@ def update_device(id):
     return flask.jsonify({'success': True})
 
 @app.route('/devices/<id>', methods=['DELETE'])
+@cross_origin()
 def delete_device(id):
     _ensure_device(id)
     DEVICES.child(id).delete()
